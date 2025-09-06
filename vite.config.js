c825b1505a15b20071b589e565b30505f1a12af6
Shadowmataj <christianmataj1@gmail.com>
2025-09-06 13:47:22 -0600

    fix ports

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: 'http://localhost:80/api',
+        target: 'http://localhost:5000/api',
         changeOrigin: true,
         secure: false,
         ws: true
